dbe2e1e46cd4a1e1e31e798daf43bcb6057d27fd
ktotah <ket2137@columbia.edu>
2023-12-22 01:35:49 -0800

    edited comments in my script.js file in order to increase the quality of the comments. also fixed a typo in my includeLowercase validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,40 @@
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-
 // Define the generatePassword function
 function generatePassword(){
 
+
   // DETERMINING PASSWORD LENGTH
-  // prompt user for password length and parse it to an integer
+
+  // Prompt user for password length and parse it to an integer
   let passwordLength = parseInt(prompt("Enter the number of characters for your password (8-128):")); 
 
   //Validate password length
   while(isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-    passwordLength = parseInt(prompt("Password length must be an integer between 8 and 128 characters. How many characters should the password be?")) //paswordLength won't be officially set until the user inputs a valid integer 
+    passwordLength = parseInt(prompt("Password length must be an integer between 8 and 128 characters. How many characters should the password be?")) // Repeat prompt until a valid integer between 8 and 128 is entered
   } 
 
+
   // DETERMING POSSIBLE CHARACTERS FOR PASSWORD
-  // Confirm character types to include
+
+  // Confirm inclusion of various character types
   let includeLowercase = confirm("Include lowercase letters?");
   let includeUppercase = confirm("Include uppercase letters?");
   let includeNumeric = confirm("Include numbers?");
   let includeSpecialCharacters = confirm("Include special characters?");
 
-  // Validate character type selection (at least one must be selected)
+  // Validate that at least one character type is selected and, if not, repeat confirm functions until at least one character type is selected
   while (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecialCharacters) {
     alert("At least one character type must be selected.");
-    includeLowercase = confirm("Include lowercase letter?");
+    includeLowercase = confirm("Include lowercase letters?");
     includeUppercase = confirm("Include uppercase letters?");
     includeNumeric = confirm("Include numbers?");
     includeSpecialCharacters = confirm("Include special characters?");
   }
 
   // Initialize arrays for each character type
-  // using `split('')` to convert each string of characters into an array of individual characters
+  // Using `split('')` to convert each string of characters into an array of individual characters
   const lowercaseArray = 'abcdefghijklmnopqrstuvwxyz'.split('');
   const uppercaseArray = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
   const numbersArray = '0123456789'.split('');
@@ -56,24 +58,28 @@ function generatePassword(){
     possibleCharacters = possibleCharacters.concat(specialArray);
   }
   
+
   // GENERATING FINAL PASSWORD
+
   // Initializing a variable to store the final password result
   let finalPassword = "";
 
-  // creating a for loop that runs as many times as the length of the password set by the user (starting at 0 and incrementing after each iteration until it reaches `passwordLength`) 
+  // Loop to create a password of the user-defined length
   for (let i=0; i < passwordLength; i++){
-    // using Math.floor(Math.random() * possibleCharacters.length) to set a value for the randomIndex for placement i in the password based on rounding down the value of the random number generated between 0 (inclusive) & 1 (exclusive)multiplied by the length of our possibleCharacters array in order to get a random number to correspond to a value in the array
-    // i.e., say the array has a length of 10 (as if the user only selected numeric characters) and the random number generated is 0.99. Math.floor(0.99 * 10) === Math.floor(9.9) --> which rounds down to the nearest whole number which, in this case, is 9. Thus, randomIndex = 9.
+    // Generate a random index to select a character from possibleCharacters
+    // Math.random() randomly generates a number between 0 (inclusive) and 1 (exclusive)
+    // Multiplying by possibleCharacters.length scales the randomly generated number to the array length
+    // Math.floor() rounds down to the nearest whole number, producing a valid array index
     const randomIndex = Math.floor(Math.random() * possibleCharacters.length);
 
-    // Adding the character at `randomIndex` in the `possibleCharacters` array to the right-hand side of the final password
-    // i.e., continuing the above example, where the password only contains numeric characters from our array ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] (and keeping in mind that arrays are zero-indexed), possibleCharacter[9] = 9, so 9 is the value that will be added to the right-hand side of the final passwod
+    // Append the character at randomIndex from possibleCharacters to finalPassword
     finalPassword += possibleCharacters[randomIndex];
   }
-  // after the loop completes (i.e., after `passwordLength` iterations), this line returns the final, randomly-generated password string. 
+  // Returning the final, randomly-generated password string after the loop completes (i.e., after `passwordLength` iterations)
   return finalPassword;
 }
 
+// Write password to the #password input
 function writePassword() {
   const password = generatePassword();
   const passwordText = document.querySelector("#password");
